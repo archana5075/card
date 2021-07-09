9308126b00f2c8695421a5a9397e688786e0f588
Archana Potnis <potnis.archana@gmail.com>
2021-07-08 22:49:51 -0700

    Added API call to create multiple cards

--- fetchCards.js
+++ fetchCards.js
@@ -1,39 +1,56 @@
-async function fetchPodcasts() {
+var index = 0;
+async function fetchPodcasts(index = 0) {
   const response = await fetch("cards.json");
   let cards = await response.json();
 
-  cards["cards"].map((card) => {
-    document.querySelector(".container").innerHTML += `
+  document.querySelector(".container").innerHTML = `
       <div class="card">
         <div class="card-header">
             <div class="div-photo">
-                <img src="${card.profileImg}" />
+                <img src="${cards["cards"][index].profileImg}" />
             </div> 
         </div>  
         <div class="card-address">
            <div class="div-name">
-              <h1><span class="name">${card.name}</span> <span class="age">${card.age}</span></h1> 
+              <h1><span class="name">${cards["cards"][index].name}</span> <span class="age">${cards["cards"][index].age}</span></h1> 
            </div>
-           <div class="div-city"><h2 class="city" >${card.city}</h2></div>
+           <div class="div-city"><h2 class="city" >${cards["cards"][index].city}</h2></div>
         </div>
         <div class="card-footer">
             <div class="div-small-box">
-                <div class="div-number">${card.followersCount}</div>
+                <div class="div-number">${cards["cards"][index].followersCount}</div>
                 <div class="div-text">Followers</div>
             </div>
             <div class="div-small-box">
-                <div class="div-number">${card.likesCount}</div>
+                <div class="div-number">${cards["cards"][index].likesCount}</div>
                 <div class="div-text"> Likes</div>
             </div>
             <div class="div-small-box">
-                <div class="div-number">${card.photosCount}</div>
+                <div class="div-number">${cards["cards"][index].photosCount}</div>
                 <div class="div-text">Photos</div>
             </div>
         </div>
-
-      </div>  
+       
+      </div>
+      
         `;
-  });
-}
 
-fetchPodcasts();
+  return cards["cards"];
+}
+document.getElementById("loadmore").onclick = () => {
+  let cardsCount = fetchPodcasts(index);
+  cardsCount.then(function (result) {
+    fetchPodcasts(index);
+    index++;
+
+    //console.log(result.length);
+    console.log(index);
+    if (index === result.length) {
+      index = 0;
+      //console.log(index);
+      fetchPodcasts(index);
+    }
+  });
+};
+
+fetchPodcasts(0);
